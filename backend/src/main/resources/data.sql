ba6ce978d9ece61a532e48c7ed5b33f5e1d40d35
Daniel Silva <daniel.mascr@gmail.com>
2022-02-08 14:22:57 -0300

    Lessons seed, fix: lessonsDone

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -28,3 +28,19 @@ INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prer
 
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2022-02-01T13:00:00Z', null, true, false);
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2022-02-01T13:00:00Z', null, true, false);
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 1 do primeiro capítulo', 1, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (1, 'Material de apoio', 'https://apoio');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 2 do primeiro capítulo', 2, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (2, 'Material promocional', 'https://apoio');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 3 do primeiro capítulo', 3, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (3, '', 'https://apoio');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Tarefa do primeiro capítulo', 4, 1);
+INSERT INTO tb_task (id, description, question_Count, approval_Count, weight, due_Date) VALUES (4, 'Trabalho para entregar', 5, 4, 1.0, TIMESTAMP WITH TIME ZONE '2022-02-25T13:00:00Z');
+
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (1, 1, 1);
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (2, 1, 1);
+
